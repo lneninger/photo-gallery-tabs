9ddec9e0e1e92a4d2b06f757cdf9e68130f98772
Leonardo <lneninger@hipalanet.com>
2023-08-04 08:57:08 -0400

    Fixing refresher

--- src/app/menu-categories/menu-categories.page.ts
+++ src/app/menu-categories/menu-categories.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, RefresherCustomEvent } from '@ionic/angular';
 import { GalleryService } from '../shared/camera/gallery.service';
 import { GalleryImageOptions, GalleryPhoto } from '@capacitor/camera';
 import { CommonModule } from '@angular/common';
@@ -25,7 +25,8 @@ export class MenuCategoriesPage implements OnInit {
     this.menus = await this.service.getMenus();
   }
 
-  async refresh($event?: Event): Promise<void> {
+  async refresh($event: any): Promise<void> {
     this.menus = await this.service.getMenus();
+    await $event.target.complete();
   }
 }
